edcec1c395502ce16a06e5645b8d24e74459d86f
LordBH <fugg@ukr.net>
2016-02-13 21:29:53 +0200

    add migrations

---
+++ configurations/filters.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+def online(time):
+    now = datetime.now()
+    full_time = str(time.hour) + ':' + str(time.minute if time.minute/10 > 1 else '0' + str(time.minute))
+    full_date = str(time.day) + '.' + str(time.month) + '.' + str(time.year)
+
+    if now.date() > time.date():
+        if now.day == time.day + 1:
+            return 'Last seen yesterday at ' + full_time
+
+        return 'Last seen ' + full_date
+
+    if now.hour - 3 > time.hour:
+        return 'Last seen ' + str(now.hour - time.hour) + ' hours'
+    elif now.hour > time.hour:
+        return 'Last seen at ' + full_time + ' today'
+
+    if now.minute > time.minute:
+        return 'Last seen ' + str(now.minute - time.minute) + ' min'
+
+    return 'Last seen ' + str(now.second - time.second) + 'sec'
+
+
+filters = (
+
+    online,
+
+)
