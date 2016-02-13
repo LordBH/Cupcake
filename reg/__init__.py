0592d9e176830ae6251d0a9337574621485ee609
LordBH <fugg@ukr.net>
2016-02-13 21:17:38 +0200

    re-init

--- reg/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-
-from_reg = Blueprint('reg', __name__, template_folder='templates',
-                     static_folder='static', static_url_path='/%s' % __name__)
-
-
-from . import views
