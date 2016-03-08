63abb5fb864fce9661a5eba188e82b91fc26347d
LordBH <lordbh@ukr.net>
2016-03-08 13:59:03 +0200

    delete Rooms

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from run_app import app, db
-from models.models import User, ActivatedUsers
-from models.chat import Rooms
+from models.models import User, ActivatedUsers, Rooms
 
 
 migrate = Migrate(app, db)
@@ -11,4 +10,3 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-
