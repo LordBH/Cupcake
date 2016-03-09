8cd1646ffbba6d1c9d6570b980a5ced0376d534b
LordBH <lordbh@ukr.net>
2016-03-09 19:42:07 +0200

    fix recursive problem with 'joined'

--- chats/routes.py
+++ chats/routes.py
@@ -24,25 +24,24 @@ def joined(data):
 
     room_id = '%d|%d' % extra
 
-    if session.get('rooms') is None:
-        session['rooms'] = []
+    if room_id not in session['rooms']:
+        join_room(room_id)
+        session['rooms'].append(room_id)
+        emit('unique_wire', {'flag': True, 'id': user_2, 'user': user_1}, room=user_2)
+        save_room(user_1, user_2, room=room_id)
 
-    join_room(room_id)
-    session['rooms'].append(room_id)
-    emit('unique_wire', {'flag': True, 'id': user_2, 'user': user_1}, room=user_2)
+    if session.setdefault('flag_for_joined', True):
+        session['flag_for_joined'] = False
 
-    save_room(user_1, user_2, room=room_id)
+        chat = take_message(room_id, extra)
 
-    chat = take_message(room_id, extra)
+        context = {
+            'flag': True,
+            'room': room_id,
+            'history': chat,
+        }
 
-    context = {
-        'flag': True,
-        'room': room_id,
-        'msg': session.get('user_first_name') + ' joined ' + room_id,
-        'history': chat,
-    }
-
-    return emit('status', context, room=room_id)
+        emit('status', context, room=room_id)
 
 
 @socket_io.on('message', namespace='/chat')
